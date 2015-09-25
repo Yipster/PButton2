2e9863a6dd2bf803e8a04dfc478d93b72585de84
BY <byipster@hotmail.com>
2015-09-24 22:46:28 -0600

    updated
    made the bad clouds make you reset the storyboard
    also added a count and highscore to it

--- PeterButton/MainWindow.xaml.cs
+++ PeterButton/MainWindow.xaml.cs
@@ -23,9 +23,9 @@ namespace PeterButton
     {
 
         Storyboard s;
-        cloud cloud1;
-        cloud cloud2;
-        cloud cloud3;
+        int countnum;
+        int highscorenum;
+        int newhigh;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +34,8 @@ namespace PeterButton
             var sb = this.Resources["ClassroomStoryboard"];
             s = sb as Storyboard;
             s.Begin();
+            countnum = 0;
+            highscorenum = 0;
 
         }
 
@@ -94,52 +96,68 @@ namespace PeterButton
             {
                 case 1:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/nexuscloud.png"));
-                    
+                    countnum++;
                     break;
                 case 2:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/honkscloud.png"));
-                    
+                    countnum++;
                     break;
                 case 3:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/catcloud.png"));
                     uri = new Uri(@"../../Resources/kitten.wav", UriKind.Relative);
                     PlaySound(uri);
+                    countnum++;
                     break;
 
                 case 4:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/snoocloud.png"));
+                    countnum++;
                     break;
 
                 case 5:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/suzuyacloud.png"));
                     uri = new Uri(@"../../Resources/Suzuya.wav", UriKind.Relative);
                     PlaySound(uri);
+                    countnum++;
                     break;
 
                 case 6:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/yuicloud.png"));
                     uri = new Uri(@"../../Resources/Yahallo.wav", UriKind.Relative);
                     PlaySound(uri);
+                    countnum++;
                     break;
                 case 7:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/UX.png"));
+                    countnum++;
                     break;
                 case 8:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/pc.png"));
+                    countnum++;
                     break;
                 case 9:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/spidercloud.png"));
                     uri = new Uri(@"../../Resources/thunder.wav", UriKind.Relative);
                     PlaySound(uri);
                     s.Seek(TimeSpan.Zero);
+                    newhigh = countnum;
+                    countnum = 0;
                     break;
                 case 10:
                     cloud.Img.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/celerycloud.png"));
                     uri = new Uri(@"../../Resources/thunder.wav", UriKind.Relative);
                     PlaySound(uri);
                     s.Seek(TimeSpan.Zero);
+                    newhigh = countnum;
+                    countnum = 0;
                     break;
-            } 
+            }
+            count.Text = countnum.ToString();
+            if (newhigh > highscorenum)
+            {
+                highscorenum = newhigh;
+                highscore.Text = highscorenum.ToString();
+            }
         }
 
         public int RandomIntGenerator(int i)
